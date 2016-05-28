a8b75aa0248946cbaea855d9bc97c590da23a847
eiurur <sigsig_kbeatodenmania@yahoo.co.jp>
2016-05-28 11:26:46 +0900

    first commit

---
+++ test/abnormal.js
@@ -0,0 +1,9 @@
+'use strict';
+
+const assert = require('assert');
+
+describe('abnormal', () => {
+  it('1 + 1 !== 3', () => {
+    assert(1 + 1 !== 3);
+  });
+});
---
+++ test/normal.js
@@ -0,0 +1,9 @@
+'use strict';
+
+const assert = require('assert');
+
+describe('normal', () => {
+  it('1 + 1 === 2', () => {
+    assert(1 + 1 === 2);
+  });
+});
